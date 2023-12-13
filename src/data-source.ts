e40489160944450010d5bbc765bce25f82a6e4f9
Juan Camilo Hincapie <juchincapiegi@unal.edu.co>
2023-12-13 15:46:51 +0100

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
